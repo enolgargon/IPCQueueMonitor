14a57e757212743524edaf293dba80a90276ff88
enolgargon <enolgargon@gmail.com>
2019-09-29 00:39:51 +0200

    App component designed
    Created a base design for index component with a list of monitors and a button that creates others.
    
    Signed-off-by: enolgargon <enolgargon@gmail.com>

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import {Component} from '@angular/core';
+import {PosixMonitorComponent} from './posix-monitor/posix-monitor.component';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,13 @@ import {Component} from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'IPCQueueMonitor';
+  monitors = [];
+
+  constructor() {
+    this.createMonitor();
+  }
+
+  createMonitor() {
+    this.monitors.push(new PosixMonitorComponent());
+  }
 }
